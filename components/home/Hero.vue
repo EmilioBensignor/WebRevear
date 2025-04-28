dd2aa2a2a4f12d6361d9e149ea2de82a765128f9
Emilio Bensignor <liobensignor@gmail.com>
2025-04-28 12:55:15 -0300

    Responsive entre media queries

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -3,7 +3,7 @@
         <DefaultContent class="!max-w-[1920px]">
             <picture>
                 <source media="(min-width: 1440px)" srcset="/images/home/banners/Revear-Hero-DesktopXL.png" />
-                <source media="(min-width: 1080px)" srcset="/images/home/banners/Revear-Hero-Desktop.png" />
+                <source media="(min-width: 660px)" srcset="/images/home/banners/Revear-Hero-Desktop.png" />
                 <NuxtImg src="/images/home/banners/Revear-Hero-Mobile.png" alt="Experto Revear"
                     class="w-full object-cover" />
             </picture>
