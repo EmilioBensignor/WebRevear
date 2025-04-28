f6b8997d9da87795746d461b43a958b83bffb77b
Emilio Bensignor <liobensignor@gmail.com>
2025-04-28 16:23:24 -0300

    Iconos social media + Banners en webp

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -2,9 +2,9 @@
     <DefaultSection>
         <DefaultContent class="!max-w-[1920px]">
             <picture>
-                <source media="(min-width: 1440px)" srcset="/images/home/banners/Revear-Hero-DesktopXL.png" />
-                <source media="(min-width: 660px)" srcset="/images/home/banners/Revear-Hero-Desktop.png" />
-                <NuxtImg src="/images/home/banners/Revear-Hero-Mobile.png" alt="Experto Revear"
+                <source media="(min-width: 1440px)" srcset="/images/home/banners/Revear-Hero-DesktopXL.webp" />
+                <source media="(min-width: 660px)" srcset="/images/home/banners/Revear-Hero-Desktop.webp" />
+                <NuxtImg src="/images/home/banners/Revear-Hero-Mobile.webp" alt="Experto Revear"
                     class="w-full object-cover" />
             </picture>
         </DefaultContent>
